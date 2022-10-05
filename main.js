151b988d37e6aa7d616a19bad8c25fb633ee4cae
JurRodi <rodijurrien@gmail.com>
2022-10-05 16:34:52 +0200

    Feat: content disappear on click

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ import './style.css'
 import Weather from './src/Weather.js'
 
 const weather = new Weather('7ea25643391a4de38fb63330220510');
+
+// on click h1 element, clear app and load new content
+const h1 = document.querySelector('h1');
+h1.addEventListener('click', () => {
+   document.querySelector('#app').innerHTML = '';
+    
+});
