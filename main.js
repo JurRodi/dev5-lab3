bbda2b109d0ffa55aa12a3b7cd3e99c6447fe078
JurRodi <rodijurrien@gmail.com>
2022-10-05 12:05:31 +0200

    Feat: basic weather app (made in lesson)

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 import './style.css'
+import Weather from './src/Weather.js'
+
+const weather = new Weather('7ea25643391a4de38fb63330220510');
---
+++ src/Weather.js
@@ -0,0 +1,51 @@
+export default class Weather {
+    constructor(apiKey) {
+        this.apiKey = apiKey;
+        
+        if (localStorage.getItem('weather') && Date.now() - localStorage.getItem('timestamp') < 600000) {
+            const data = JSON.parse(localStorage.getItem('weather'));
+            this.displayWeather(data);
+        } else {
+            this.getLocation();
+        }
+    }
+
+    getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.getWeather.bind(this));
+        } else {
+            alert('Geolocation is not supported by this browser.');
+        }
+    }
+
+    getWeather(position) {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        console.log(lat, lon);
+
+        const url = `https://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${lat},${lon}&aqi=no`;
+        
+        fetch(url)
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data)
+                localStorage.setItem('weather', JSON.stringify(data));
+                localStorage.setItem('timestamp', Date.now());
+
+                this.displayWeather(data);
+            });
+    }
+
+    displayWeather(data) {
+        const temp = data.current.temp_c;
+        document.querySelector(".weather__temp").innerHTML = temp + "°C";
+
+        const weather = data.current.condition.text;
+        document.querySelector(".weather__summary").innerHTML = weather;
+
+        const icon = data.current.condition.icon;
+        const img = document.createElement('img');
+        img.src = icon;
+        document.querySelector(".weather__icon").appendChild(img);
+    }
+}
